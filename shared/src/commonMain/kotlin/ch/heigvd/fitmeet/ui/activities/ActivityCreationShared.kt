2fa30ad2ca8ac762542faed916efe262e878091d
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-09-01 10:53:02 +0200

    activity type

--- shared/src/commonMain/kotlin/ch/heigvd/fitmeet/ui/activities/ActivityCreationShared.kt
+++ shared/src/commonMain/kotlin/ch/heigvd/fitmeet/ui/activities/ActivityCreationShared.kt
@@ -3,6 +3,7 @@ package ch.heigvd.fitmeet.ui.activities
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import ch.heigvd.fitmeet.ui.theme.Level
 
 var currentScreen = mutableStateOf(0)
 
@@ -15,7 +16,7 @@ class ActivityData {
     var position by mutableStateOf("")
     var name by mutableStateOf("")
     var description by mutableStateOf("")
-    var difficulty by mutableStateOf("any")
+    var difficulty by mutableStateOf(Level.ALL)
 }
 
 var activityData = ActivityData()
