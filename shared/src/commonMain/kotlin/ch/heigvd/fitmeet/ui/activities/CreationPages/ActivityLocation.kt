ea1be6ab742de319b4e1f9549adecbee89cbc728
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-09-01 17:27:18 +0200

    useless imports

--- shared/src/commonMain/kotlin/ch/heigvd/fitmeet/ui/activities/CreationPages/ActivityLocation.kt
+++ shared/src/commonMain/kotlin/ch/heigvd/fitmeet/ui/activities/CreationPages/ActivityLocation.kt
@@ -1,23 +1,6 @@
 package ch.heigvd.fitmeet.ui.activities
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ch.heigvd.fitmeet.ui.map.MapScreen
 
 @Composable
